6859f6431529bdcc2b9a10dd5449241e35ea50a3
ljh <ljh@example.com>
2019-01-22 15:40:33 +0800

    使用inputstream传入keystore，修复keystore在jar包中无法导入 删除工厂类，由调用方处理

--- src/main/java/pub/ljh/wcpay/WeChat.java
+++ src/main/java/pub/ljh/wcpay/WeChat.java
@@ -2,9 +2,10 @@ package pub.ljh.wcpay;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.Security;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import lombok.Builder;
+import lombok.Getter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import pub.ljh.wcpay.client.HttpClient;
 import pub.ljh.wcpay.client.HttpsClient;
@@ -16,25 +17,26 @@ public class WeChat {
 		Security.addProvider(new BouncyCastleProvider());
 	}
 
+	@Getter
 	private final String appid;
-
-	private final String secret;
-
+	@Getter
 	private final String mchId;
-
-	private final String key;
-
-	private final File keyStore;
 
 	private Pay pay = null;
 	private Sns sns = null;
 
-	private WeChat(String appid, String secret, String mchId, String key, File keyStore) {
+	public Pay pay() {
+		return pay;
+	}
+
+	public Sns sns() {
+		return sns;
+	}
+
+	@Builder(builderMethodName = "with")
+	public WeChat(String appid, String secret, String mchId, String key, InputStream keyStore) {
 		this.appid = appid;
-		this.secret = secret;
 		this.mchId = mchId;
-		this.key = key;
-		this.keyStore = keyStore;
 
 		HttpClient client = null;
 		if (mchId != null && key != null) {
@@ -51,91 +53,6 @@ public class WeChat {
 			}
 			sns = new Sns(client, appid, secret);
 		}
-	}
-
-	public static Builder with() {
-		return new Builder();
-	}
-
-	public static class Builder {
-
-		private String appid;
-
-		private String secret;
-
-		private String mchId;
-
-		private String key;
-
-		private File keyStore;
-
-		public Builder Appid(String appid) {
-			this.appid = appid;
-			return this;
-		}
-
-		public Builder Secret(String secret) {
-			this.secret = secret;
-			return this;
-		}
-
-		public Builder MchId(String mchId) {
-			this.mchId = mchId;
-			return this;
-		}
-
-		public Builder Key(String key) {
-			this.key = key;
-			return this;
-		}
-
-		public Builder KeyStore(File keyStore) {
-			this.keyStore = keyStore;
-			return this;
-		}
-
-		public WeChat build() {
-			return new WeChat(appid, secret, mchId, key, keyStore);
-		}
-
-	}
-
-	public static LocalDateTime parse(String time) {
-		if (time.matches("^\\d{14}$")) {
-			return LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-		}
-		if (time.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$")) {
-			return LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-		}
-		return null;
-	}
-
-	public Pay pay() {
-		return pay;
-	}
-
-	public Sns sns() {
-		return sns;
-	}
-
-	public String getAppid() {
-		return appid;
-	}
-
-	public String getSecret() {
-		return secret;
-	}
-
-	public String getMchId() {
-		return mchId;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public File getKeyStore() {
-		return keyStore;
 	}
 
 }
